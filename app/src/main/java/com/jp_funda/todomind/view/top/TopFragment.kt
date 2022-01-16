4db229e84fc8bba0ea0d37c61d6f6198a8f19c0a
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-16 20:42:47 +0900

    Change kotlin version to use jetpack compose

--- app/src/main/java/com/jp_funda/todomind/view/top/TopFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/top/TopFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
 import com.jp_funda.todomind.R
 
 class TopFragment : Fragment() {
@@ -20,13 +24,15 @@ class TopFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.top_fragment, container, false)
+        viewModel = ViewModelProvider(this)[TopViewModel::class.java]
+//        return inflater.inflate(R.layout.top_fragment, container, false)
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Text(
+                    text = "Jet pack",
+                    color = Color.White
+                )
+            }
+        }
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(TopViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
