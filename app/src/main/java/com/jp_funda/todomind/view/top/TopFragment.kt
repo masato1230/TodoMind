fe88b64d0c2de4722adfda3b336ed178b1c2ac64
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-16 22:24:54 +0900

    Add top base design

--- app/src/main/java/com/jp_funda/todomind/view/top/TopFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/top/TopFragment.kt
@@ -6,10 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.jp_funda.todomind.R
 
 class TopFragment : Fragment() {
@@ -28,11 +44,78 @@ class TopFragment : Fragment() {
 //        return inflater.inflate(R.layout.top_fragment, container, false)
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(
-                    text = "Jet pack",
-                    color = Color.White
-                )
+                TopContent()
             }
         }
+    }
+
+    @Composable
+    fun TopContent() {
+
+        val scrollState = rememberScrollState()
+
+        Column(
+            modifier = Modifier.verticalScroll(scrollState)
+        ) {
+            // Section Projects
+            Text(
+                text = "Mind Maps",
+                modifier = Modifier.padding(
+                    top = 40.dp,
+                    bottom = 20.dp,
+                    start = 20.dp,
+                    end = 20.dp
+                ),
+                color = Color.White,
+                style = MaterialTheme.typography.h6
+            )
+
+            LazyRow(modifier = Modifier.padding(bottom = 40.dp)) {
+                // todo fill with data
+                items(items = List<String>(5) { "d" }) { str ->
+                    MindMapCard()
+                }
+            }
+
+            // Section Todos
+            Text(
+                text = "Tasks",
+                modifier = Modifier.padding(15.dp),
+                color = Color.White,
+                style = MaterialTheme.typography.h6,
+            )
+
+            TaskList()
+        }
+    }
+
+    // TopComponents
+
+    @Composable
+    fun MindMapCard() {
+        AndroidView(factory = {
+            View.inflate(it, R.layout.card_mind_map, null)
+        })
+    }
+
+    @Composable
+    fun TaskList() {
+        Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+            List(5) { "d" }.forEach { str ->
+                TaskRow()
+            }
+        }
+    }
+
+    @Composable
+    fun TaskRow() {
+        AndroidView(
+            factory = {
+                View.inflate(it, R.layout.row_task, null)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp),
+        )
     }
 }
