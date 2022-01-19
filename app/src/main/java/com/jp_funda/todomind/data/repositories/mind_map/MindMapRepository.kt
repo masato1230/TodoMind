18bee8066c8ccf0a568a2710b970e709fc7209e2
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-20 08:22:54 +0900

    Add createMindMap function

--- app/src/main/java/com/jp_funda/todomind/data/repositories/mind_map/MindMapRepository.kt
+++ app/src/main/java/com/jp_funda/todomind/data/repositories/mind_map/MindMapRepository.kt
@@ -1,7 +1,25 @@
 package com.jp_funda.todomind.data.repositories.mind_map
 
+import com.jp_funda.todomind.data.repositories.mind_map.entity.MindMap
+import io.reactivex.rxjava3.core.Single
+import io.realm.Realm
+
 class MindMapRepository {
-    // TODO crate
+
+    fun createMindMap(mindMap: MindMap): Single<MindMap> {
+        return Single.create { emitter ->
+            Realm.getDefaultInstance().executeTransactionAsync({ realm ->
+                realm.insert(mindMap)
+            }, {
+                emitter.onSuccess(mindMap)
+            }, {
+                emitter.onError(it)
+            })
+        }
+    }
+
+    
+
     // TODO getAllMindMaps
     // TODO getMindMap
     // TODO update MindMap
