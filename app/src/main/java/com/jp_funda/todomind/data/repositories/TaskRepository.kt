80effb80cc8a476aa07f34d2544bf1f53ed43dfb
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-19 22:50:02 +0900

    Add createTask function

--- app/src/main/java/com/jp_funda/todomind/data/repositories/TaskRepository.kt
+++ app/src/main/java/com/jp_funda/todomind/data/repositories/TaskRepository.kt
@@ -1,11 +1,27 @@
 package com.jp_funda.todomind.data.repositories
 
 import com.jp_funda.todomind.data.database.dataentities.Task
+import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Single
 import io.realm.Realm
 import io.realm.kotlin.where
 
 class TaskRepository {
+
+    fun createTask(task: Task): Single<Task> {
+        return Single.create<Task> { emitter ->
+            Realm.getDefaultInstance().executeTransactionAsync({ realm ->
+                realm.insert(task)
+            }, {
+                emitter.onSuccess(task)
+            }, { error ->
+                emitter.onError(error)
+            })
+        }
+    }
+
+    // TODO create updateTask()
+    // TODO create deleteTask()
 
     fun getAllTasks(): Single<List<Task>> {
         var result = emptyList<Task>()
