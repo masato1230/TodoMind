cb22ff0d57f7624443c904345a633698b67b8e68
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-22 13:33:42 +0900

    Add frame of MindMapContent

--- app/src/main/java/com/jp_funda/todomind/view/components/MindMapCard.kt
+++ app/src/main/java/com/jp_funda/todomind/view/components/MindMapCard.kt
@@ -2,12 +2,16 @@ package com.jp_funda.todomind.view.components
 
 import android.view.View
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.jp_funda.todomind.R
 
 @Composable
-fun MindMapCard() {
-    AndroidView(factory = {
-        View.inflate(it, R.layout.card_mind_map, null)
-    })
+fun MindMapCard(modifier: Modifier = Modifier) {
+    AndroidView(
+        factory = {
+            View.inflate(it, R.layout.card_mind_map, null)
+        },
+        modifier = modifier
+    )
 }
