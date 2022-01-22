9253ffcd4acc078039619099f30a71f6fb833c9c
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-22 12:35:06 +0900

    Add task fragment layout

---
+++ app/src/main/java/com/jp_funda/todomind/view/components/TaskRow.kt
@@ -0,0 +1,22 @@
+package com.jp_funda.todomind.view.components
+
+import android.view.View
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.jp_funda.todomind.R
+
+@Composable
+fun TaskRow() {
+    AndroidView(
+        factory = {
+            View.inflate(it, R.layout.row_task, null)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp),
+    )
+}
--- app/src/main/java/com/jp_funda/todomind/view/task/TaskFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/task/TaskFragment.kt
@@ -6,7 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jp_funda.todomind.R
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jp_funda.todomind.view.components.TaskRow
 
 class TaskFragment : Fragment() {
 
@@ -20,13 +33,64 @@ class TaskFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.task_fragment, container, false)
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        return ComposeView(requireContext()).apply {
+            setContent {
+                TaskContent()
+            }
+        }
     }
 
+    @Preview
+    @Composable
+    fun TaskContent() {
+        var selectedTabIndex by remember { mutableStateOf(0) }
+
+        Column {
+            TabRow(
+                selectedTabIndex = selectedTabIndex,
+                backgroundColor = Color.Black,
+                contentColor = Color.White,
+            ) {
+                Tab(
+                    selected = selectedTabIndex == 0,
+                    onClick = {
+                        // todo add filtering
+                        selectedTabIndex = 0
+                    },
+                    text = { Text("Open") }
+                )
+                Tab(
+                    selected = selectedTabIndex == 1,
+                    onClick = {
+                        // todo add filtering
+                        selectedTabIndex = 1
+                    },
+                    text = { Text("In Progress") }
+                )
+                Tab(
+                    selected = selectedTabIndex == 2,
+                    onClick = {
+                        // todo add filtering
+                        selectedTabIndex = 2
+                    },
+                    text = { Text("Closed") }
+                )
+            }
+
+            TaskList()
+        }
+    }
+
+    @Composable
+    fun TaskList() {
+        LazyColumn(modifier = Modifier.padding(horizontal = 20.dp)) {
+            // todo fill with data
+            items(items = List(10) { "d" }) { str ->
+                TaskRow()
+            }
+        }
+    }
 }
