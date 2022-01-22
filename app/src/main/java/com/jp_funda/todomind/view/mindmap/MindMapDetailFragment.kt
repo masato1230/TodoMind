9c65c3d58eced144dc793dfe218ba18cfc6592bf
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-22 16:46:24 +0900

    Add MindMapDetailFragment

---
+++ app/src/main/java/com/jp_funda/todomind/view/mindmap/MindMapDetailFragment.kt
@@ -0,0 +1,16 @@
+package com.jp_funda.todomind.view.mindmap
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jp_funda.todomind.R
+
+class MindMapDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+}
