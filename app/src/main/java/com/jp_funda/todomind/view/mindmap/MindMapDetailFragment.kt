8058b1ee6a52da9328fe801107763a358a270fc5
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-23 10:59:20 +0900

    Add mind map thumbnail

--- app/src/main/java/com/jp_funda/todomind/view/mindmap/MindMapDetailFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/mindmap/MindMapDetailFragment.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.addCallback
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.findNavController
+import com.jp_funda.todomind.R
 
 class MindMapDetailFragment : Fragment() {
 
@@ -26,8 +37,34 @@ class MindMapDetailFragment : Fragment() {
         // return layout
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "kdkk")
+                MindMapDetailContent()
             }
+        }
+    }
+
+    @Composable
+    fun MindMapDetailContent() {
+        Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+
+            // Thumbnail
+            Image(
+                painter = painterResource(
+                    id = R.drawable.img_mind_map_sample // TODO change image to real mind map
+                ),
+                contentDescription = "Mind Map description",
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(20.dp)),
+                contentScale = ContentScale.Crop,
+            )
+
+            // Title
+            // Description
+            // Progress bar
+            // Progress description
+            // Created Date and Last Updated Date
+            // Task list
         }
     }
 }
