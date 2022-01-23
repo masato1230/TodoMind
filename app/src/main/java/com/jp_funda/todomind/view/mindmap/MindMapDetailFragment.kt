b9947734abbda71924a488f784c1d3f8507bb60e
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-23 19:48:49 +0900

    feat: Change base color to navy from crimson for visibility

--- app/src/main/java/com/jp_funda/todomind/view/mindmap/MindMapDetailFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/mindmap/MindMapDetailFragment.kt
@@ -10,9 +10,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
@@ -30,7 +28,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.jp_funda.todomind.R
-import com.jp_funda.todomind.view.components.TaskLists
 import com.jp_funda.todomind.view.components.TaskRow
 
 // TODO add delete button to action bar
