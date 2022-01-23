93ce34caaa1a69688c6d279f7ac7a75214b48b02
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-23 13:01:37 +0900

    feat: make detail view scrollable

--- app/src/main/java/com/jp_funda/todomind/view/mindmap/MindMapDetailFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/mindmap/MindMapDetailFragment.kt
@@ -9,10 +9,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -26,6 +31,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.jp_funda.todomind.R
 import com.jp_funda.todomind.view.components.TaskLists
+import com.jp_funda.todomind.view.components.TaskRow
 
 // TODO add delete button to action bar
 class MindMapDetailFragment : Fragment() {
@@ -50,68 +56,105 @@ class MindMapDetailFragment : Fragment() {
     @Preview
     @Composable
     fun MindMapDetailContent() {
-        Column(modifier = Modifier.padding(horizontal = 20.dp)) {
-
-            // Thumbnail Section
-            Image(
-                painter = painterResource(
-                    id = R.drawable.img_mind_map_sample // TODO change image to real mind map
-                ),
-                contentDescription = "Mind Map description",
-                modifier = Modifier
-                    .height(200.dp)
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(20.dp)),
-                contentScale = ContentScale.Crop,
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
-
-            // Description Section
-            // Title
-            Text(
-                text = "Mind Map Title",
-                modifier = Modifier.padding(bottom = 10.dp),
-                style = MaterialTheme.typography.h6,
-                color = Color.White
-            ) // TODO add click listener to edit view
-
-            // Description
-            Text(
-                text = "This is description of the mind map. this is description of mind map. this is description of mind map",
-                modifier = Modifier.padding(bottom = 10.dp),
-                style = MaterialTheme.typography.body2,
-                color = Color.LightGray,
-            ) // TODO add click listener to edit view
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-            // Progress Section
-            // Progress description
-            Row(
-                modifier = Modifier
-                    .padding(start = 10.dp, bottom = 5.dp)
-                    .fillMaxWidth(),
-            ) {
-                Text(
-                    text = "Progress: ",
-                    style = MaterialTheme.typography.body1,
-                    color = Color.White
+        LazyColumn(modifier = Modifier
+            .padding(horizontal = 20.dp)) {
+            item {
+                // Thumbnail Section
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.img_mind_map_sample // TODO change image to real mind map
+                    ),
+                    contentDescription = "Mind Map description",
+                    modifier = Modifier
+                        .height(200.dp)
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop,
                 )
-                Spacer(modifier = Modifier.width(10.dp))
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                // Description Section
+                // Title
                 Text(
-                    text = "70%",
-                    style = MaterialTheme.typography.body1,
+                    text = "Mind Map Title",
+                    modifier = Modifier.padding(bottom = 10.dp),
+                    style = MaterialTheme.typography.h6,
                     color = Color.White
-                )
+                ) // TODO add click listener to edit view
+
+                // Description
+                Text(
+                    text = "This is description of the mind map. this is description of mind map. this is description of mind map",
+                    modifier = Modifier.padding(bottom = 10.dp),
+                    style = MaterialTheme.typography.body2,
+                    color = Color.LightGray,
+                ) // TODO add click listener to edit view
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                // Progress Section
+                // Progress description
+                Row(
+                    modifier = Modifier
+                        .padding(start = 10.dp, bottom = 5.dp)
+                        .fillMaxWidth(),
+                ) {
+                    Text(
+                        text = "Progress: ",
+                        style = MaterialTheme.typography.body1,
+                        color = Color.White
+                    )
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(
+                        text = "70%",
+                        style = MaterialTheme.typography.body1,
+                        color = Color.White
+                    )
+                }
+                // Progress bar
+                RoundedProgressBar(percent = 70)
+
+                // Date Section
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                // Task list Section
+                var selectedTabIndex by remember { mutableStateOf(0) }
+                TabRow(
+                    selectedTabIndex = selectedTabIndex,
+                    backgroundColor = colorResource(id = R.color.deep_purple),
+                    contentColor = Color.White,
+                ) {
+                    Tab(
+                        selected = selectedTabIndex == 0,
+                        onClick = {
+                            // todo add filtering
+                            selectedTabIndex = 0
+                        },
+                        text = { Text("In Progress") }
+                    )
+                    Tab(
+                        selected = selectedTabIndex == 1,
+                        onClick = {
+                            // todo add filtering
+                            selectedTabIndex = 1
+                        },
+                        text = { Text("Open") }
+                    )
+                    Tab(
+                        selected = selectedTabIndex == 2,
+                        onClick = {
+                            // todo add filtering
+                            selectedTabIndex = 2
+                        },
+                        text = { Text("Closed") }
+                    )
+                }
             }
-            // Progress bar
-            RoundedProgressBar(percent = 70)
-
-            // Date Section
-
-            // Task list Section
-            TaskLists(listPadding = 0)
+            items(items = List(10) { "d" }) { str ->
+                TaskRow()
+            }
         }
     }
 
