07df10ddd1e779bbe4297ab7af66d2aa529a1651
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-30 12:02:48 +0900

    Remove extra imports

--- app/src/main/java/com/jp_funda/todomind/view/task_detail/TaskDetailFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/task_detail/TaskDetailFragment.kt
@@ -1,41 +1,17 @@
 package com.jp_funda.todomind.view.task_detail
 
-import android.app.DatePickerDialog
-import android.content.Context
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.DatePicker
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.window.DialogProperties
-import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.viewModels
-import com.google.android.material.datepicker.MaterialDatePicker
-import com.google.android.material.timepicker.MaterialTimePicker
-import com.jp_funda.todomind.R
 import com.jp_funda.todomind.view.components.TimePickerDialog
 import com.jp_funda.todomind.view.components.DatePickerDialog
-import com.vanpra.composematerialdialogs.MaterialDialog
-import com.vanpra.composematerialdialogs.MaterialDialogState
-import com.vanpra.composematerialdialogs.datetime.date.DatePickerColors
-import com.vanpra.composematerialdialogs.datetime.date.DatePickerDefaults
-import com.vanpra.composematerialdialogs.datetime.date.datepicker
-import com.vanpra.composematerialdialogs.datetime.time.TimePickerDefaults
-import com.vanpra.composematerialdialogs.datetime.time.timepicker
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import dagger.hilt.android.AndroidEntryPoint
-import java.time.Instant.now
-import java.util.*
 
 @AndroidEntryPoint
 class TaskDetailFragment : Fragment() {
@@ -59,14 +35,12 @@ class TaskDetailFragment : Fragment() {
 
     @Composable
     fun TaskDetailContent() {
-//        Button(onClick = { showDatePicker(requireActivity()) }) {
-//            Text("Hey")
-//        }
+        // Set up dialogs
         val dateDialogState = rememberMaterialDialogState()
         val timeDialogState = rememberMaterialDialogState()
         DatePickerDialog(dateDialogState, resources)
         TimePickerDialog(timeDialogState, resources)
 //        dateDialogState.show()
-        timeDialogState.show()
+//        timeDialogState.show()
     }
 }
