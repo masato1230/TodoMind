9b33de0e8b660c0635661ded0086dbc9093aec14
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-01-30 13:27:59 +0900

    Add basic layout

--- app/src/main/java/com/jp_funda/todomind/view/task_detail/TaskDetailFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/task_detail/TaskDetailFragment.kt
@@ -5,13 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
+import com.jp_funda.todomind.R
+import com.jp_funda.todomind.data.repositories.task.entity.TaskStatus
 import com.jp_funda.todomind.view.components.TimePickerDialog
 import com.jp_funda.todomind.view.components.DatePickerDialog
 import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.compose.material.ExperimentalMaterialApi as ExperimentalMaterialApi1
 
 @AndroidEntryPoint
 class TaskDetailFragment : Fragment() {
@@ -22,6 +39,7 @@ class TaskDetailFragment : Fragment() {
 
     private val taskDetailViewModel by viewModels<TaskDetailViewModel>()
 
+    @ExperimentalMaterialApi1
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,6 +51,8 @@ class TaskDetailFragment : Fragment() {
         }
     }
 
+    @ExperimentalMaterialApi1
+    @Preview
     @Composable
     fun TaskDetailContent() {
         // Set up dialogs
@@ -42,5 +62,160 @@ class TaskDetailFragment : Fragment() {
         TimePickerDialog(timeDialogState, resources)
 //        dateDialogState.show()
 //        timeDialogState.show()
+
+        // Set up TextFields color
+        val colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.White,
+            backgroundColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            cursorColor = Color(resources.getColor(R.color.teal_200)),
+        )
+
+        Column(modifier = Modifier.padding(horizontal = 20.dp)) {
+            // Page Title
+            Text(
+                text = "Create New Task",
+                color = Color.White,
+                style = MaterialTheme.typography.h5,
+            ) // TODO change by create/edit
+
+            // Title TextField
+            TextField(
+                colors = colors,
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(
+                        text = "Enter title",
+                        color = Color.Gray,
+                        style = MaterialTheme.typography.h6,
+                    )
+                }) // TODO update memory
+
+            // Description TextField
+            TextField(
+                colors = colors,
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "Add description", color = Color.Gray)
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_notes_24dp),
+                        contentDescription = "Description",
+                        tint = Color.White
+                    )
+                })
+
+            // Date & Time
+            Row(modifier = Modifier.fillMaxWidth()) {
+                // Date
+                TextField(
+                    colors = colors,
+                    modifier = Modifier.fillMaxWidth(0.5f),
+                    value = "",
+                    onValueChange = {},
+                    placeholder = {
+                        Text(text = "Due date/time", color = Color.Gray)
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.DateRange,
+                            tint = Color.White,
+                            contentDescription = "Date",
+                        )
+                    },
+                    readOnly = true,
+                )
+
+                // Time TODO show only when date is filled
+                TextField(
+                    colors = colors,
+                    modifier = Modifier.fillMaxWidth(),
+                    value = "",
+                    onValueChange = {},
+                    placeholder = {
+                        Text(text = "Add time", color = Color.Gray)
+                    },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_alarm_24dp),
+                            tint = Color.White,
+                            contentDescription = "Time",
+                        )
+                    },
+                    readOnly = true,
+                )
+            }
+
+            // Color
+            TextField(
+                colors = colors,
+                modifier = Modifier.fillMaxWidth(),
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "Set color", color = Color.Gray)
+                },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_color_24dp),
+                        tint = Color(resources.getColor(R.color.teal_200)), // TODO use task color
+                        contentDescription = "Color",
+                    )
+                },
+                readOnly = true,
+            )
+
+            // Status
+            val statusOptions = TaskStatus.values()
+            var expanded by remember { mutableStateOf(false) }
+            var selectedStatus by remember { mutableStateOf(statusOptions[0]) }
+
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = {
+                    expanded = !expanded
+                }
+            ) {
+                TextField(
+                    colors = colors,
+                    value = "Status - ${selectedStatus.name}",
+                    onValueChange = {},
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Check,
+                            contentDescription = "Status",
+                            tint = Color.White
+                        )
+                    },
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = expanded
+                        )
+                    },
+                    readOnly = true,
+                )
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = {
+                        expanded = false
+                    }) {
+                    statusOptions.forEach { option ->
+                        DropdownMenuItem(onClick = {
+                            selectedStatus = option
+                            expanded = false
+                        }) {
+                            Text(text = option.name)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
