2a9e2b1716c93788fff2d531e8779122ebcebb34
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-02-12 10:43:55 +0900

    Add MindMap Entity

--- app/src/main/java/com/jp_funda/todomind/view/mind_map_detail/MindMapDetailViewModel.kt
+++ app/src/main/java/com/jp_funda/todomind/view/mind_map_detail/MindMapDetailViewModel.kt
@@ -1,13 +1,20 @@
 package com.jp_funda.todomind.view.mind_map_detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.jp_funda.todomind.data.repositories.mind_map.MindMapRepository
+import com.jp_funda.todomind.data.repositories.mind_map.entity.MindMap
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class MindMapDetailViewModel @Inject constructor(
     private val mindMapRepository: MindMapRepository
 ) : ViewModel() {
+    private var _mindMap = MutableLiveData(MindMap(createdDate = Date()))
+    val mindMap: LiveData<MindMap> = _mindMap
+
     var isEditing: Boolean = false
 }
