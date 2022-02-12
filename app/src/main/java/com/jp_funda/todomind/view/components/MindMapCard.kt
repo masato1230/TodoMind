073d5c9a949fca13b3b4edb56ffd9b43d5b6fec9
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-02-12 18:41:15 +0900

    Add layout id

--- app/src/main/java/com/jp_funda/todomind/view/components/MindMapCard.kt
+++ app/src/main/java/com/jp_funda/todomind/view/components/MindMapCard.kt
@@ -8,12 +8,20 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 import com.jp_funda.todomind.R
+import com.jp_funda.todomind.data.repositories.mind_map.entity.MindMap
 
 @Composable
-fun MindMapCard(onClick: () -> Unit, modifier: Modifier = Modifier) {
+fun MindMapCard(
+    mindMap: MindMap,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
     AndroidView(
         factory = {
             View.inflate(it, R.layout.card_mind_map, null)
+        },
+        update = { view ->
+            // Initialize view
         },
         modifier = modifier.clickable {
             onClick()
