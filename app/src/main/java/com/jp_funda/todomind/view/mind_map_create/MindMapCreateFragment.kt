a0369838f1026c55a968fed691bc7549e345ca5b
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-02-19 21:32:36 +0900

    Add navigation to mind map creation

---
+++ app/src/main/java/com/jp_funda/todomind/view/mind_map_create/MindMapCreateFragment.kt
@@ -0,0 +1,31 @@
+package com.jp_funda.todomind.view.mind_map_create
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jp_funda.todomind.databinding.FragmentMindMapCreateBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MindMapCreateFragment : Fragment() {
+
+    private var _binding: FragmentMindMapCreateBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMindMapCreateBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
