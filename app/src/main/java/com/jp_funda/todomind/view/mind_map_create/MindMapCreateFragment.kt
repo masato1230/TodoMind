d1bff6bccf8f7aaea5409bf53e36b81c7779260f
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-03-02 08:40:59 +0900

    Add navigation to sheet

--- app/src/main/java/com/jp_funda/todomind/view/mind_map_create/MindMapCreateFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/mind_map_create/MindMapCreateFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.jp_funda.todomind.R
 import com.jp_funda.todomind.databinding.FragmentMindMapCreateBinding
 import com.jp_funda.todomind.view.MainViewModel
@@ -92,7 +93,7 @@ class MindMapCreateFragment : Fragment() {
                     mindMap = mindMapCreateViewModel.mindMap,
                     viewModel = mindMapCreateViewModel,
                 ) {
-                    // todo onClick
+                    findNavController().navigate(R.id.navigation_mind_map_options_dialog)
                 }
 
 //                H1(
