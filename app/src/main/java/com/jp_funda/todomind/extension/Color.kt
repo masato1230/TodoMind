dba8d60a9a112389af9c869bf00f5c72b98e733f
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-03-05 15:00:59 +0900

    Add luminance extension

---
+++ app/src/main/java/com/jp_funda/todomind/extension/Color.kt
@@ -0,0 +1,8 @@
+package com.jp_funda.todomind.extension
+
+import androidx.compose.ui.graphics.Color
+
+/** Calculate luminance in range of 0 to 1 */
+fun Color.getLuminance(): Float {
+    return ((0.299 * red + 0.587 * green + 0.114 * blue) / 255).toFloat()
+}
