9ab6f217f31482962ed292b842a54ca322246dc5
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-03-12 09:27:30 +0900

    Add settings base layout

--- app/src/main/java/com/jp_funda/todomind/view/settings/SettingsViewModel.kt
+++ app/src/main/java/com/jp_funda/todomind/view/settings/SettingsViewModel.kt
@@ -1,7 +1,10 @@
 package com.jp_funda.todomind.view.settings
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SettingsViewModel : ViewModel() {
+@HiltViewModel
+class SettingsViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
