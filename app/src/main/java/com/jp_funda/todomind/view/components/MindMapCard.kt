a724065a302a569833a448e37421e2adaf47a650
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-03-13 11:44:13 +0900

    Add background chage functionality to mindmap card

--- app/src/main/java/com/jp_funda/todomind/view/components/MindMapCard.kt
+++ app/src/main/java/com/jp_funda/todomind/view/components/MindMapCard.kt
@@ -1,5 +1,6 @@
 package com.jp_funda.todomind.view.components
 
+import android.graphics.Color
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -7,6 +8,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.luminance
 import com.jp_funda.todomind.R
 import com.jp_funda.todomind.data.repositories.mind_map.entity.MindMap
 
@@ -21,11 +23,19 @@ fun MindMapCard(
             View.inflate(it, R.layout.card_mind_map, null)
         },
         update = { view ->
+            val tintColor =
+                if (mindMap.color?.luminance ?: 0f > 0.6) Color.BLACK else Color.WHITE
+
             // Initialize view
+            val background = view.findViewById<View>(R.id.map_card_background)
             val createdDate = view.findViewById<TextView>(R.id.map_card_created_date)
             val title = view.findViewById<TextView>(R.id.map_card_title)
-            val progressPercentageText = view.findViewById<TextView>(R.id.map_card_progress_percentage)
+            val progressPercentageText =
+                view.findViewById<TextView>(R.id.map_card_progress_percentage)
             val progressBar = view.findViewById<ProgressBar>(R.id.map_card_progress_bar)
+
+            // card background
+            mindMap.color?.let { background.setBackgroundColor(it) }
 
             // created date
             mindMap.createdDate?.let {
@@ -35,6 +45,7 @@ fun MindMapCard(
             }
             // title
             title.text = mindMap.title ?: ""
+            title.setTextColor(tintColor)
             // TODO progressPercentageText
             // TODO progressBar
         },
