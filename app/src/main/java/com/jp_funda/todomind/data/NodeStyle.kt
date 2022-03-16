49ee6fde2fc9c0cbd5955e2f47395bb63d4eefb4
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-03-17 08:08:19 +0900

    Add h4

--- app/src/main/java/com/jp_funda/todomind/data/NodeStyle.kt
+++ app/src/main/java/com/jp_funda/todomind/data/NodeStyle.kt
@@ -10,6 +10,7 @@ enum class NodeStyle(val title: String) {
     HEADLINE_1(title = "Headline 1"),
     HEADLINE_2(title = "Headline 2"),
     HEADLINE_3(title = "Headline 3"),
+    HEADLINE_4(title = "Headline 4"),
     BODY_1(title = "Body 1"),
     BODY_2(title = "Body 2"),
 }
@@ -19,8 +20,9 @@ fun NodeStyle.getSize(): Size {
         NodeStyle.HEADLINE_1 -> Size(250f, 250f)
         NodeStyle.HEADLINE_2 -> Size(220f, 220f)
         NodeStyle.HEADLINE_3 -> Size(170f, 170f)
-        NodeStyle.BODY_1 -> Size(800f, 30f)
-        NodeStyle.BODY_2 -> Size(800f, 25f)
+        NodeStyle.HEADLINE_4 -> Size(150f, 150f)
+        NodeStyle.BODY_1 -> Size(500f, 30f)
+        NodeStyle.BODY_2 -> Size(500f, 25f)
     }
 }
 
@@ -39,6 +41,7 @@ fun NodeStyle.getTextSize(): TextUnit {
         NodeStyle.HEADLINE_1 -> MaterialTheme.typography.h4.fontSize
         NodeStyle.HEADLINE_2 -> MaterialTheme.typography.h4.fontSize * 0.9
         NodeStyle.HEADLINE_3 -> MaterialTheme.typography.h4.fontSize * 0.6
+        NodeStyle.HEADLINE_4 -> MaterialTheme.typography.h4.fontSize * 0.6
         NodeStyle.BODY_1 -> MaterialTheme.typography.h5.fontSize
         NodeStyle.BODY_2 -> MaterialTheme.typography.h6.fontSize
     }
--- app/src/main/java/com/jp_funda/todomind/view/components/MindMapCreateContent.kt
+++ app/src/main/java/com/jp_funda/todomind/view/components/MindMapCreateContent.kt
@@ -33,7 +33,8 @@ fun MindMapCreateContent(
                 when (task.styleEnum) {
                     NodeStyle.HEADLINE_1 -> H1(task = task, viewModel = mindMapCreateViewModel) { onClickTaskNode(task) }
                     NodeStyle.HEADLINE_2 -> H2(task = task, viewModel = mindMapCreateViewModel) { onClickTaskNode(task) }
-                    NodeStyle.HEADLINE_3 -> H2(task = task, viewModel = mindMapCreateViewModel) { onClickTaskNode(task) }
+                    NodeStyle.HEADLINE_3 -> H3(task = task, viewModel = mindMapCreateViewModel) { onClickTaskNode(task) }
+                    NodeStyle.HEADLINE_4 -> H4(task = task, viewModel = mindMapCreateViewModel) { onClickTaskNode(task) }
                     NodeStyle.BODY_1 -> Body1(task = task, viewModel = mindMapCreateViewModel) { onClickTaskNode(task) }
                     NodeStyle.BODY_2 -> Body2(task = task, viewModel = mindMapCreateViewModel) { onClickTaskNode(task) }
                 }
