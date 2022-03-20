f9c40ba21237bdea7df0d8ac2c9645b2097ef598
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-03-20 12:30:24 +0900

    Add isShowOgpThumbnail field

--- app/src/main/java/com/jp_funda/todomind/view/mind_map_create/options_sheet/AddChildViewModel.kt
+++ app/src/main/java/com/jp_funda/todomind/view/mind_map_create/options_sheet/AddChildViewModel.kt
@@ -3,6 +3,7 @@ package com.jp_funda.todomind.view.mind_map_create.options_sheet
 import com.jp_funda.todomind.data.repositories.mind_map.entity.MindMap
 import com.jp_funda.todomind.data.repositories.ogp.OgpRepository
 import com.jp_funda.todomind.data.repositories.task.TaskRepository
+import com.jp_funda.todomind.data.shared_preferences.SettingsPreferences
 import com.jp_funda.todomind.view.task_detail.TaskEditableViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -11,7 +12,8 @@ import javax.inject.Inject
 class AddChildViewModel @Inject constructor(
     taskRepository: TaskRepository,
     ogpRepository: OgpRepository,
-) : TaskEditableViewModel(taskRepository, ogpRepository) {
+    settingsPreferences: SettingsPreferences,
+) : TaskEditableViewModel(taskRepository, ogpRepository, settingsPreferences) {
     fun setMindMap(mindMap: MindMap) {
         _task.value!!.mindMap = mindMap
     }
--- app/src/main/java/com/jp_funda/todomind/view/mind_map_create/options_sheet/EditTaskViewModel.kt
+++ app/src/main/java/com/jp_funda/todomind/view/mind_map_create/options_sheet/EditTaskViewModel.kt
@@ -2,6 +2,7 @@ package com.jp_funda.todomind.view.mind_map_create.options_sheet
 
 import com.jp_funda.todomind.data.repositories.ogp.OgpRepository
 import com.jp_funda.todomind.data.repositories.task.TaskRepository
+import com.jp_funda.todomind.data.shared_preferences.SettingsPreferences
 import com.jp_funda.todomind.view.task_detail.TaskEditableViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,4 +11,5 @@ import javax.inject.Inject
 class EditTaskViewModel @Inject constructor(
     taskRepository: TaskRepository,
     ogpRepository: OgpRepository,
-) : TaskEditableViewModel(taskRepository, ogpRepository) {}
+    settingsPreferences: SettingsPreferences,
+) : TaskEditableViewModel(taskRepository, ogpRepository, settingsPreferences)
