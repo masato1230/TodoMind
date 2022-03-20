149c03cf6b0ae9df86458b1ce609e4bf668aa264
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-03-20 10:52:05 +0900

    Add settings preference util

---
+++ app/src/main/java/com/jp_funda/todomind/data/shared_preferences/SettingsPreferences.kt
@@ -0,0 +1,30 @@
+package com.jp_funda.todomind.data.shared_preferences
+
+import android.content.Context
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SettingsPreferences @Inject constructor(context: Context) {
+    companion object {
+        const val PREF_NAME = "Settings"
+    }
+
+    private val preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+
+    fun getFloat(key: PreferenceKeys): Float {
+        return preferences.getFloat(key.key, 0f)
+    }
+
+    fun setFloat(key: PreferenceKeys, value: Float) {
+        preferences.edit().putFloat(key.key, value).apply()
+    }
+
+    fun getBoolean(key: PreferenceKeys): Boolean {
+        return preferences.getBoolean(key.key, false)
+    }
+
+    fun setBoolean(key: PreferenceKeys, value: Boolean) {
+        preferences.edit().putBoolean(key.key, value).apply()
+    }
+}
--- app/src/main/java/com/jp_funda/todomind/di/AppModule.kt
+++ app/src/main/java/com/jp_funda/todomind/di/AppModule.kt
@@ -1,17 +1,25 @@
 package com.jp_funda.todomind.di
 
+import android.content.Context
 import com.jp_funda.todomind.data.repositories.mind_map.MindMapRepository
 import com.jp_funda.todomind.data.repositories.ogp.OgpRepository
 import com.jp_funda.todomind.data.repositories.task.TaskRepository
+import com.jp_funda.todomind.data.shared_preferences.SettingsPreferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSettingsPreferences(@ApplicationContext appContext: Context) =
+        SettingsPreferences(appContext)
 
     @Provides
     @Singleton
