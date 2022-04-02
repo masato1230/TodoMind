917a1ba85dd68fd024bb8aaf68c4ec5a26928e46
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-02 22:14:44 +0900

    Add introduction settings field

--- app/src/main/java/com/jp_funda/todomind/view/components/IntroPage.kt
+++ app/src/main/java/com/jp_funda/todomind/view/components/IntroPage.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -36,6 +37,7 @@ fun IntroPage(
                 .padding(horizontal = 20.dp)
                 .fillMaxWidth(),
             style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
         )
 
