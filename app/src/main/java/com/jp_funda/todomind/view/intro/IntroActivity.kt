44d685e74067d31afb37850dfbbfe492e605f075
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-02 16:31:37 +0900

    Add bottom button

--- app/src/main/java/com/jp_funda/todomind/view/intro/IntroActivity.kt
+++ app/src/main/java/com/jp_funda/todomind/view/intro/IntroActivity.kt
@@ -3,11 +3,16 @@ package com.jp_funda.todomind.view.intro
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.colorResource
@@ -52,25 +57,49 @@ class IntroActivity : AppCompatActivity() {
                 contentPadding = PaddingValues(horizontal = 32.dp),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(0.9f)
+                    .fillMaxHeight(0.8f)
             ) { page ->
                 Text("android", color = Color.White)
             }
+            
+            Spacer(modifier = Modifier.weight(1f))
 
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.1f),
+                horizontalArrangement = Arrangement.Center,
             ) {
                 HorizontalPagerIndicator(
                     pagerState = pagerState,
-                    activeColor = Color.LightGray,
+                    activeColor = colorResource(id = R.color.teal_200),
                     inactiveColor = Color.DarkGray,
-                    indicatorWidth = 16.dp,
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxHeight(0.1f),
+                    indicatorHeight = 5.dp,
+                    indicatorWidth = 24.dp,
+                    spacing = 10.dp,
                 )
             }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .widthIn(500.dp)
+                    .height(50.dp)
+                    .clip(RoundedCornerShape(1000.dp))
+                    .background(Color.White),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Next",
+                    color = Color.Black,
+                    style = MaterialTheme.typography.h6,
+                )
+            }
+            
+            Spacer(modifier = Modifier.height(20.dp))
         }
     }
 }
