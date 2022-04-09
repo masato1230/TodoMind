b54ec037dbeb3a1d32d34f2b9611d4616f459446
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-09 13:47:18 +0900

    Add pref field for create screen tutoria

--- app/src/main/java/com/jp_funda/todomind/data/shared_preferences/PreferenceKeys.kt
+++ app/src/main/java/com/jp_funda/todomind/data/shared_preferences/PreferenceKeys.kt
@@ -3,6 +3,7 @@ package com.jp_funda.todomind.data.shared_preferences
 enum class PreferenceKeys(val key: String) {
     // settings
     IS_NOT_FIRST_TIME_LAUNCH(key = "is not first time launch"),
+    IS_NOT_FIRST_TIME_CREATE_SCREEN(key = "is not first time create screen"),
     DEFAULT_MIND_MAP_SCALE(key = "default mind map scale"),
     IS_SHOW_OGP_THUMBNAIL(key = "is show ogp thumbnail"),
     IS_REMIND_TASK_DEADLINE("is remind task deadline"),
--- app/src/main/java/com/jp_funda/todomind/view/mind_map_create/MindMapCreateFragment.kt
+++ app/src/main/java/com/jp_funda/todomind/view/mind_map_create/MindMapCreateFragment.kt
@@ -19,6 +19,8 @@ import com.jp_funda.todomind.R
 import com.jp_funda.todomind.data.NodeStyle
 import com.jp_funda.todomind.data.getSize
 import com.jp_funda.todomind.data.repositories.mind_map.entity.MindMap
+import com.jp_funda.todomind.data.shared_preferences.PreferenceKeys
+import com.jp_funda.todomind.data.shared_preferences.SettingsPreferences
 import com.jp_funda.todomind.databinding.FragmentMindMapCreateBinding
 import com.jp_funda.todomind.view.MainViewModel
 import com.jp_funda.todomind.view.components.LineContent
@@ -119,8 +121,12 @@ class MindMapCreateFragment : Fragment() {
         }
         mindMapCreateViewModel.isLoading.observe(viewLifecycleOwner, loadingObserver)
 
-        // todo Show tutorial dialog for first time
-        showTutorialDialog()
+        // Show tutorial dialog for first time
+        val settingsPreferences = SettingsPreferences(requireContext())
+        if (!settingsPreferences.getBoolean(PreferenceKeys.IS_NOT_FIRST_TIME_CREATE_SCREEN)) {
+            showTutorialDialog()
+            settingsPreferences.setBoolean(PreferenceKeys.IS_NOT_FIRST_TIME_CREATE_SCREEN, true)
+        }
 
         return binding.root
     }
