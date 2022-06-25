6ba74647c26fda44a66f519a4cc4970543153c92
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-25 17:47:43 +0900

    Add bottom bar menu items

---
+++ app/src/main/java/com/jp_funda/todomind/navigation/NavigationRoutes.kt
@@ -0,0 +1,10 @@
+package com.jp_funda.todomind.navigation
+
+object NavigationRoutes {
+    // Bottom navigation
+    const val Top = "top"
+    const val Task = "task"
+    const val MindMap = "mind map"
+    const val Record = "record"
+    const val Settings = "settings"
+}
