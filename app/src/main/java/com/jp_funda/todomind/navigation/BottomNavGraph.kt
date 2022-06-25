9e5c9f53b49502bb31690eb18c538fd89b23effc
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-25 18:53:19 +0900

    Add empty route navigations

--- app/src/main/java/com/jp_funda/todomind/navigation/BottomNavGraph.kt
+++ app/src/main/java/com/jp_funda/todomind/navigation/BottomNavGraph.kt
@@ -1,10 +1,12 @@
 package com.jp_funda.todomind.navigation
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 
 @ExperimentalAnimationApi
 @Composable
@@ -17,6 +19,38 @@ fun BottomNavGraph(
         startDestination = NavigationRoutes.Top
     ) {
         /** Home Screen. */
+        composable(route = BottomBarMenuItem.Top.route) {
+            bottomBarState.value = true
+            // TODO
+            Text(text = BottomBarMenuItem.Top.route)
+        }
 
+        /** Task Screen. */
+        composable(route = BottomBarMenuItem.Task.route) {
+            bottomBarState.value = true
+            // TODO
+            Text(text = BottomBarMenuItem.Task.route)
+        }
+
+        /** MindMap Screen. */
+        composable(route = BottomBarMenuItem.MindMap.route) {
+            bottomBarState.value = true
+            // TODO
+            Text(text = BottomBarMenuItem.MindMap.route)
+        }
+
+        /** Record Screen. */
+        composable(route = BottomBarMenuItem.Record.route) {
+            bottomBarState.value = true
+            // TODO
+            Text(text = BottomBarMenuItem.Record.route)
+        }
+
+        /** Settings Screen. */
+        composable(route = BottomBarMenuItem.Settings.route) {
+            bottomBarState.value = true
+            // TODO
+            Text(text = BottomBarMenuItem.Settings.route)
+        }
     }
 }
--- app/src/main/java/com/jp_funda/todomind/view/MainScreen.kt
+++ app/src/main/java/com/jp_funda/todomind/view/MainScreen.kt
@@ -8,9 +8,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -28,11 +30,14 @@ fun MainScreen() {
     val navController = rememberAnimatedNavController()
     val bottomBarState = remember { mutableStateOf(true) }
 
-    Scaffold(bottomBar = {
-        if (bottomBarState.value) {
-            BottomBar(navController = navController)
-        }
-    }) {
+    Scaffold(
+        bottomBar = {
+            if (bottomBarState.value) {
+                BottomBar(navController = navController)
+            }
+        },
+        backgroundColor = colorResource(id = R.color.deep_purple),
+    ) {
         BottomNavGraph(navController, bottomBarState)
     }
 }
@@ -67,7 +72,14 @@ fun RowScope.AddItem(
     navController: NavController,
 ) {
     BottomNavigationItem(
-        label = { Text(text = stringResource(id = menuItem.titleRes)) },
+        label = {
+            Text(
+                text = stringResource(id = menuItem.titleRes),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontSize = MaterialTheme.typography.caption.fontSize * 0.8,
+            )
+        },
         icon = {
             Icon(
                 painter = painterResource(id = menuItem.iconRes),
@@ -79,6 +91,8 @@ fun RowScope.AddItem(
             if (navController.currentDestination?.route != menuItem.route) {
                 navController.navigate(menuItem.route) { popUpTo(NavigationRoutes.Top) }
             }
-        }
+        },
+        selectedContentColor = colorResource(id = R.color.teal_200),
+        unselectedContentColor = Color.Black,
     )
 }
