a6bca1604b3c9bfd914a558d9c6b40ed03873c10
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-07-25 13:50:13 +0900

    Add test for GetTasksUseCase

---
+++ app/src/test/java/com/jp_funda/todomind/domain/use_cases/task/GetTaskUseCaseShould.kt
@@ -0,0 +1,21 @@
+package com.jp_funda.todomind.domain.use_cases.task
+
+import com.jp_funda.todomind.domain.repositories.TaskRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.mockito.Mockito.*
+import java.util.*
+
+@ExperimentalCoroutinesApi
+class GetTaskUseCaseShould {
+    private val repository = mock(TaskRepository::class.java)
+
+    private val taskId = UUID.randomUUID()
+
+    @Test
+    fun `call getTask function of TaskRepository`() = runTest {
+        GetTaskUseCase(repository)(taskId)
+        verify(repository, times(1)).getTask(taskId)
+    }
+}
