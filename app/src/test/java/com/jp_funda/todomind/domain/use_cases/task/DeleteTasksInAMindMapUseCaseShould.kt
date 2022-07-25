2eeb025fa0c5b4ee8b4d93c709de76d99d6e2603
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-07-25 09:02:49 +0900

    Add test for DeleteTasksInAMindMapUseCase

---
+++ app/src/test/java/com/jp_funda/todomind/domain/use_cases/task/DeleteTasksInAMindMapUseCaseShould.kt
@@ -0,0 +1,21 @@
+package com.jp_funda.todomind.domain.use_cases.task
+
+import com.jp_funda.todomind.data.repositories.mind_map.entity.MindMap
+import com.jp_funda.todomind.domain.repositories.TaskRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.mockito.Mockito.*
+
+@ExperimentalCoroutinesApi
+class DeleteTasksInAMindMapUseCaseShould {
+    private val repository = mock(TaskRepository::class.java)
+
+    private val mindMap = MindMap()
+
+    @Test
+    fun `call deleteTasksInAMindMap function of TaskRepository`() = runTest {
+        DeleteTasksInAMindMapUseCase(repository)(mindMap)
+        verify(repository, times(1)).deleteTasksInAMindMap(mindMap)
+    }
+}
