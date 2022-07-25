981140ae94e7792c80e47ad83d02f6f280c15bc0
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-07-25 13:47:02 +0900

    Add test for GetTasksInAMindMapUseCase

---
+++ app/src/test/java/com/jp_funda/todomind/domain/use_cases/task/GetTasksInAMindMapUseCaseShould.kt
@@ -0,0 +1,21 @@
+package com.jp_funda.todomind.domain.use_cases.task
+
+import com.jp_funda.todomind.data.repositories.mind_map.entity.MindMap
+import com.jp_funda.todomind.domain.repositories.TaskRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.mockito.Mockito.*
+
+@ExperimentalCoroutinesApi
+class GetTasksInAMindMapUseCaseShould {
+    private val repository = mock(TaskRepository::class.java)
+
+    private val mindMap = MindMap()
+
+    @Test
+    fun `call getTasksInAMindMap function of TaskRepository`() = runTest {
+        GetTasksInAMindMapUseCase(repository)(mindMap)
+        verify(repository, times(1)).getTasksInAMindMap(mindMap)
+    }
+}
